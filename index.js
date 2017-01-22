1a56a6bcb2a0d8c0390f5de178c5e41508e137fb
Yuriy Kurylenko <yuriy.kurylenko@gmail.com>
2017-01-22 15:58:11 +0100

    Initial JWT.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var request = require('superagent');
+var jwt = require('jsonwebtoken');
 var _ = require('lodash');
 
 var services = {
@@ -7,17 +8,16 @@ var services = {
     domain: 'localhost:9292',
     endpoints: [{
       method: 'get',
-      path: ''
-    }, {
-      method: 'post',
-      path: '/login'
+      path: '',
+      roles: ['admin']
     }]
   },
   tasks: {
     domain: 'localhost:8000',
     endpoints: [{
       method: 'get',
-      path: ''
+      path: '',
+      roles: ['user', 'admin']
     }]
   }
 };
@@ -27,16 +27,22 @@ server.use(restify.bodyParser({ mapParams: true }));
 
 function registerEndpoint(serviceName, domain, endpoint) {
   return function(req, res, next) {
-    var chain = request[endpoint.method](domain + '/' + serviceName + endpoint.path);
-    if (_.includes(['post', 'put', 'patch'], endpoint.method) && req.body) {
-      chain = chain.send(JSON.parse(req.body));
-    }
-    chain.end(function(error, result) {
+    jwt.verify(req.header('Authorization'), 'secret', function(error, decoded) {
       if (error) {
-        res.send(error.status);
-      } else if (result) {
-        var json = JSON.parse(result.text);
-        res.send(result.status, json);
+        res.send(401);
+      } else if (decoded) {
+        var chain = request[endpoint.method](domain + '/' + serviceName + endpoint.path);
+        if (_.includes(['post', 'put', 'patch'], endpoint.method) && req.body) {
+          chain = chain.send(JSON.parse(req.body));
+        }
+        chain.end(function(error, result) {
+          if (error) {
+            res.send(error.status);
+          } else if (result) {
+            var json = JSON.parse(result.text);
+            res.send(result.status, json);
+          }
+        });
       }
     });
   };
@@ -46,12 +52,33 @@ function registerServices() {
   _.each(_.keys(services), function(key) {
     var service = services[key];
     _.each(service.endpoints, function(endpoint) {
-      server[endpoint.method](key + endpoint.path, registerEndpoint(key, service.domain, endpoint));
+      server[endpoint.method](
+        key + endpoint.path,
+        registerEndpoint(key, service.domain, endpoint)
+      );
     });
   });
 }
 
+function login(req, res, next) {
+  request.post(services.users.domain + '/users/login')
+    .send(JSON.parse(req.body))
+    .end(function(error, result) {
+      if (error) {
+        res.send(error.status);
+      } else if (result) {
+        var json = JSON.parse(result.text);
+        json.token = jwt.sign({
+          data: json.id
+        }, 'secret', { expiresIn: 60 * 60 });
+        res.send(result.status, json);
+      }
+    });
+}
+
 registerServices();
+
+server.post('/login', login);
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
